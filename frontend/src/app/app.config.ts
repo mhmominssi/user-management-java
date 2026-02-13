4879b6571a5792da3440f646ca4b9366b9713171
mhmominssi <mohammadali.momin@softshilinfotech.in>
2026-02-13 11:40:00 +0530

    UM - Updates main.ts and app.config.ts

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
 import { provideRouter } from '@angular/router';
 import { KENDO_GRID } from '@progress/kendo-angular-grid';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
+  providers: [provideAnimations(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
